6c281aaedc8e5bbb4b63b04bf34161af1ff76a91
Segun Osiki <osiki.segun@gmail.com>
2022-10-28 06:29:27 +0100

    adding id for each links

--- linktree-app/src/components/Home.jsx
+++ linktree-app/src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
         
     <>
         <section>
-           <div className="flex justify-center flex-col items-center p-0 mt-12">
-            <ProfileIcon />
+           <div className="flex justify-center flex-col items-center p-0 mt-12 ">
+            <ProfileIcon id="profile__img" />
            </div>
            <h2 className="text-gray-900 font-bold text-sm leading-7 text-center">
             Annette Black
@@ -17,37 +17,37 @@ const Home = () => {
            <div className="flex flex-col justify-center items-center py-6 px-8 gap-12">
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="www.twitter.com">Twitter Link</a>
+                    <a id="twitter" href="www.twitter.com">Twitter Link</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg items-center justify-center hidden">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="www.twitter.com">Slack Link</a>
+                    <a id="slack" href="www.twitter.com">Slack Link</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="https://training.zuri.team/">Zuri Team</a>
+                    <a id="btn__zuri" href="https://training.zuri.team/">Zuri Team</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="http://books.zuri.team">Zuri Books</a>
+                    <a id="books" href="http://books.zuri.team">Zuri Books</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="https://books.zuri.team">Python Books</a>
+                    <a id="book__python" href="https://books.zuri.team">Python Books</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="https://background.zuri.team">Background Check for Coders</a>
+                    <a id="pitch" href="https://background.zuri.team">Background Check for Coders</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a href="https://books.zuri.team/design-rules">Design Books</a>
+                    <a id="book__design" href="https://books.zuri.team/design-rules">Design Books</a>
                 </h4>
             </div>
            </div>
