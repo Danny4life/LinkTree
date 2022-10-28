8cda585562ad377b366d8f0c50a104181f1abf63
Segun Osiki <osiki.segun@gmail.com>
2022-10-28 08:33:54 +0100

    edited footer file

--- linktree-app/src/components/Home.jsx
+++ linktree-app/src/components/Home.jsx
@@ -1,23 +1,26 @@
 //import { Link } from "react-router-dom";
+import AfricaIcon from "../icons/AfricaIcon";
 import GitHub  from "../icons/GitHubIcon";
-import ProfileIcon from "../icons/ProfileIcon";
+//import ProfileIcon from "../icons/ProfileIcon";
 import SlackIcon from "../icons/SlackIcon";
+import img1 from "../images/seg.jpeg";
 
 const Home = () => {
     return ( 
         
     <>
         <section>
-           <div className="flex justify-center flex-col items-center p-0 mt-12 ">
-            <ProfileIcon id="profile__img" />
+           <div className="flex justify-center flex-col items-center p-0 mt-12">
+            {/* <ProfileIcon id="profile__img" /> */}
+            <img id="profile__img" src={img1} alt="profile_image" className="w-20 h-20 rounded-full object-fit" />
            </div>
            <h2 className="text-gray-900 font-bold text-sm leading-7 text-center">
-            Annette Black
+            Segun Osiki
            </h2>
            <div className="flex flex-col justify-center items-center py-6 px-8 gap-12">
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg flex items-center justify-center">
                 <h4 className="font-medium text-sm leading-5 text-center">
-                    <a id="twitter" href="www.twitter.com">Twitter Link</a>
+                    <a id="twitter" href="https://twitter.com/segun_osiki">Twitter Link</a>
                 </h4>
             </div>
             <div className="xl:w-3/4 w-80 h-16 bg-gray-200 border-solid rounded-lg items-center justify-center hidden">
@@ -57,6 +60,12 @@ const Home = () => {
                 <SlackIcon />
                 <GitHub />
             </div>
+            <div className="flex justify-around mt-20">
+                <h4 className="flex text-black font-bold">Zuri<div className="w-1 h-1 bg-red-700 rounded-full mt-3"></div>Internship</h4>
+                
+                <p className="text-base font-normal leading-6 text-gray-500">HNG Internship 9 Frontend Task</p>
+                <AfricaIcon />
+           </div>
         </footer>
     </>
         
