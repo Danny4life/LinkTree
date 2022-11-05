566e1e54016d7d20754798c80de34903b48e00c0
Segun Osiki <osiki.segun@gmail.com>
2022-11-05 08:19:29 +0100

    edit footer page

--- linktree-app/src/components/Footer.jsx
+++ linktree-app/src/components/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
     return ( 
      
         <footer className="">
-            <hr className="mt-24 w-10/12 m-auto xl:w-8/12 xl:relative xl:left-9" />
-            <div className="flex justify-around mt-6 flex-col ml-6 xl:flex xl:justify-around lg:flex-row">
-                <h4 className="flex text-black font-bold mb-2 xl:ml-32">Zuri
+            <hr className="mt-24 w-10/12 m-auto" />
+            <div className="flex justify-around mt-6 flex-col ml-6 xl:flex xl:justify-around xl:relative xl:right-14 lg:flex-row">
+                <h4 className="flex text-black font-bold mb-2">Zuri
                     <div className="w-1 h-1 bg-red-700 rounded-full mt-3"></div>
                         Internship
                 </h4>
                 
-                <p className="text-base font-normal leading-6 text-gray-500 mb-2">HNG Internship 9 Frontend Task</p>
-                <AfricaIcon className="xl:mr-20" />
+                <p className="text-base font-normal leading-6 text-gray-500 mb-2 xl:relative xl:left-14">HNG Internship 9 Frontend Task</p>
+                <AfricaIcon className="xl:relative xl:left-20" />
            </div>
         </footer>
        
